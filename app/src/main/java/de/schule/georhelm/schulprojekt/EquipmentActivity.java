f0ba96696159893d0494047442a4cdabd20e2df6
Georg Gar <georg.gar@gmx.de>
2018-09-20 15:29:16 +0200

    Removed unnecessary intent objects

--- app/src/main/java/de/schule/georhelm/schulprojekt/EquipmentActivity.java
+++ app/src/main/java/de/schule/georhelm/schulprojekt/EquipmentActivity.java
@@ -1,6 +1,5 @@
 package de.schule.georhelm.schulprojekt;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -14,9 +13,6 @@ public class EquipmentActivity extends AppCompatActivity {
     }
 
     public void showMenu(View v) {
-        Intent intent = new Intent(this, MenuActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-        this.startActivityIfNeeded(intent, 0);
         this.finish();
     }
 }
--- app/src/main/java/de/schule/georhelm/schulprojekt/OptionsActivity.java
+++ app/src/main/java/de/schule/georhelm/schulprojekt/OptionsActivity.java
@@ -1,9 +1,7 @@
 package de.schule.georhelm.schulprojekt;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
 import android.view.View;
 
 public class OptionsActivity extends AppCompatActivity {
@@ -14,11 +12,7 @@ public class OptionsActivity extends AppCompatActivity {
         this.setContentView(R.layout.activity_options);
     }
 
-    // doppelt in Equipment auslagern?
     public void showMenu(View v) {
-        Intent intent = new Intent(this, MenuActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-        this.startActivityIfNeeded(intent, 0);
         this.finish();
     }
 }
